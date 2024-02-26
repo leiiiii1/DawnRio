2977e12aa02d1a86b743781e8aefee059d529adf
Leah <111544542+leiiiii1@users.noreply.github.com>
2024-02-26 15:27:46 +0800

    Delete Scripts directory

--- Scripts/StatusEnum.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum StatusEnum //create a selection of status of each dawnRio
-{
-    Unaffected, Infected, Virus, Mild
-}
